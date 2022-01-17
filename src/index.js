b301d6f804df734574d20c9af9a10f118ef89b6a
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-01-17 11:43:51 +0530

    budget app

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import { BudgetsProvider } from './contexts/BudgetsContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BudgetsProvider>
+            <App />
+        </BudgetsProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
